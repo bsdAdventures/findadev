5ee54eb316b53e3087c2ed5b1a1a70655ac26417
bsdAdventures <31743169+bsdAdventures@users.noreply.github.com>
2019-11-14 15:16:03 -0600

    Delete keys.js

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://bsd_28:zdjPucBs4fTRe1Ix@paradim-cluster-0utlc.mongodb.net/test?retryWrites=true&w=majority",
-  secretKey: "nwe0cjc0scj0s9j323w9ef0ew9hfehf89wefh0ewfh8f"
-};
